d6a2d7f1d6f03e78cf795c2538ac96b861434079
bamnenim-work <bamnenim-work@bamnenim-workui-MacBookPro.local>
2024-10-19 13:19:35 -0700

    sign this message

--- src/apps/home/HomePage.tsx
+++ src/apps/home/HomePage.tsx
@@ -2,6 +2,7 @@ import { useAccount } from 'wagmi';
 import { Connector } from '../connector/Connector';
 import { History } from '../history/History';
 import { Send } from '../send/Send';
+import { Signer } from '../signer/Signer';
 
 export const HomePage = () => {
   const { address } = useAccount();
@@ -11,10 +12,12 @@ export const HomePage = () => {
       <p>This is the home page.</p>
       <Connector />
       <p>connected address is {address}</p>
+
+      <Send />
+      <Signer />
       <div>
         <History />
       </div>
-      <Send />
     </div>
   );
 };
