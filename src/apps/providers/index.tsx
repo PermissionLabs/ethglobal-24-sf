f5424db4f403a7702bd2cecc7561c0ae809e7c97
Yejun Lee <nods.dev@kakao.com>
2024-10-19 18:35:28 -0700

    Alchemy AA

--- src/apps/providers/index.tsx
+++ src/apps/providers/index.tsx
@@ -1,19 +1,29 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
-import { safeConfig } from './config';
+import { alchemyConfig, safeConfig } from './config';
 import SafeProvider from '@safe-global/safe-apps-react-sdk';
 import { SafeProvider as CustomSafeProvider } from './SafeProvider';
+import type { AlchemyClientState } from '@account-kit/core';
+import { AlchemyAccountProvider } from '@account-kit/react';
+import { AuthProvider } from './AuthProvider';
 
 const queryClient = new QueryClient();
 
-export const Providers = ({ children }: { children: React.ReactNode }) => {
+export const Providers = ({
+  children,
+  alchemyInitialState,
+}: { children: React.ReactNode; alchemyInitialState?: AlchemyClientState }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <SafeProvider>
-        <WagmiProvider config={safeConfig}>
-          <CustomSafeProvider>{children}</CustomSafeProvider>
-        </WagmiProvider>
-      </SafeProvider>
+      <AlchemyAccountProvider config={alchemyConfig} queryClient={queryClient} initialState={alchemyInitialState}>
+        <AuthProvider>
+          <SafeProvider>
+            <WagmiProvider config={safeConfig}>
+              <CustomSafeProvider>{children}</CustomSafeProvider>
+            </WagmiProvider>
+          </SafeProvider>
+        </AuthProvider>
+      </AlchemyAccountProvider>
     </QueryClientProvider>
   );
 };
